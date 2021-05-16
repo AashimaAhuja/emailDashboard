8ec4629af0f7f8eb50550555f54f57126911df2c
AashimaAhuja <aashima.ahuja@booking.com>
2021-05-16 22:49:40 +0200

    4 files modified

--- components/EmailBody.js
+++ components/EmailBody.js
@@ -1,19 +1,25 @@
-import { createElement } from "../utils/helpers";
+import { createElement } from '../utils/helpers';
 
-export const EmailBody = ({ id, initial, subject }) => {
+export const EmailBody = emailData => {
   const d = document.createDocumentFragment();
-  const avatar = document.createElement("div");
+  const avatar = document.createElement('div');
+  const initial = emailData.from.name[0].toUpperCase();
 
-  avatar.setAttribute("class", "avatar");
-  avatar.appendChild(document.createTextNode(`${initial.toUpperCase()}`));
+  console.log(emailData);
+  avatar.setAttribute('class', 'avatar');
 
-  const eleContent = createElement("div", { class: "content" });
-  const heading = createElement("div", { class: "heading" });
-  const title = createElement("div", { class: "title" });
-  const btn = createElement("button", { class: "favourite-btn" });
+  avatar.appendChild(document.createTextNode(initial));
 
-  const dateTime = createElement("div", { class: "date-time" });
-  const contentBody = createElement("div", { id: "content-body" });
+  const eleContent = createElement('div', { class: 'content' });
+  const heading = createElement('div', { class: 'heading' });
+  const title = createElement('div', { class: 'title' });
+  const btn = createElement('button', { id: 'favourite-btn' });
+
+  title.appendChild(document.createTextNode(emailData.subject));
+  btn.appendChild(document.createTextNode('Mark as favourite'));
+
+  const dateTime = createElement('div', { class: 'date-time' });
+  const contentBody = createElement('div', { id: 'content-body' });
 
   heading.appendChild(title);
   heading.appendChild(btn);
@@ -24,16 +30,6 @@ export const EmailBody = ({ id, initial, subject }) => {
 
   d.appendChild(avatar);
   d.appendChild(eleContent);
+
+  return d;
 };
-
-`<div class='avatar'></div>
-<div class= 'content'>
-  <heading>
-    <title>
-    <mark as favourite>
-  </heading>
-  <date>
-  <content-body>
-
-</div>
-`;
--- index.js
+++ index.js
@@ -1,71 +1,111 @@
 // Import stylesheets
-import "./style.css";
-import { FILTERS, BASE_URL } from "./constants";
-import fetchEmailList from "./services/emailList";
-import { EmailCard } from "./components/EmailCard";
-import fetchEmailBody from "../utils/fetchEmailBody";
-import { BASE_URL } from "../constants";
+import './style.css';
+import { FILTERS, BASE_URL } from './constants';
+import fetchEmailList from './services/emailList';
+import { EmailCard } from './components/EmailCard';
+import { EmailBody } from './components/EmailBody';
+import fetchEmailBody from './services/emailBody';
+import { BASE_URL } from '../constants';
 
-// Write Javascript code!
 (function() {
-  const appDiv = document.getElementById("app");
-  const eleMarkFavorite = document.getElementsByClassName("mark-favorite");
-  const eleCards = document.getElementsByClassName("card");
-  const favoriteList = [];
+  const emailListObj = {};
+  const emailList = document.getElementById('email-list');
 
   function init() {
     renderFilters();
     renderEmailList();
   }
 
-  async function renderEmailList() {
+  async function renderEmailList(emailList) {
     const { list } = await fetchEmailList(BASE_URL);
     const d = document.createDocumentFragment();
     list.forEach(item => {
+      emailListObj[item.id] = item;
       d.appendChild(EmailCard(item));
     });
 
-    document.getElementById("email-list").appendChild(d);
+    document.getElementById('email-list').appendChild(d);
+  }
+
+  function filterList(event) {
+    const filterName = event.target.id;
+    const d = document.createDocumentFragment();
+    const filteredList = Object.keys(emailListObj)
+      .filter(item => {
+        if (filterName == 'unread') return !emailListObj[item]['read'];
+        return emailListObj[item][filterName] == true;
+      })
+      .map(id => emailListObj[id]);
+
+    console.log(filteredList);
+    filteredList.forEach(item => {
+      d.appendChild(EmailCard(item));
+    });
+    emailList.innerHTML = '';
+    emailList.appendChild(d);
   }
 
   function renderFilters() {
     let d = document.createDocumentFragment();
     Object.keys(FILTERS).forEach(filter => {
-      let filterEle = document.createElement("div");
-      filterEle.setAttribute("id", FILTERS[filter].toLowerCase());
-      filterEle.setAttribute("class", "filter");
+      let filterEle = document.createElement('div');
+      filterEle.setAttribute('id', FILTERS[filter].toLowerCase());
+      filterEle.setAttribute('class', 'filter');
 
       var t = document.createTextNode(FILTERS[filter]);
 
       filterEle.appendChild(t);
       d.appendChild(filterEle);
     });
-    document.getElementById("filters").appendChild(d);
+    document.getElementById('filters').appendChild(d);
+  }
+
+  function markFlag(id, name) {
+    emailListObj[id][name] = true;
   }
 
   function markAsFavorite(event) {
-    const id = event.target.id;
-    favoriteList.push(id);
-    console.log(favoriteList);
+    const eleId = event.target.id;
+
+    if (eleId === 'favourite-btn') {
+      const id = event.target.closest('#email-content').dataset.id;
+      emailListObj[id].favourites = true;
+    }
   }
 
-  async function openEmail(event, id) {
-    const emailClicked = event.target;
-    emailClicked.closest(".email-card").classList.add("active");
-    console.log("hello");
+  async function openEmail(event) {
+    const emailClicked = event.target.closest('.email-card');
+    const id = emailClicked.dataset.id;
+    const eleEmailContent = document.getElementById('email-content');
+
+    eleEmailContent.innerHTML = '';
+    eleEmailContent.setAttribute('data-id', id);
+    const emailList = document.getElementById('email-list').children;
+
+    for (let ele of emailList) {
+      ele.classList.remove('active');
+    }
+
+    emailClicked.classList.add('active');
+    emailClicked.classList.add('read');
+
     const emailContent = await fetchEmailBody(`${BASE_URL}/?id=${id}`);
-    console.log(emailContent);
-    document.getElementsById("content-body").innerHTML = emailContent.body;
+    const emailData = emailListObj[id];
+    eleEmailContent.appendChild(EmailBody(emailData));
+    document.getElementById('content-body').innerHTML = emailContent.body;
+
+    document.getElementById('email-list').style.width = '35%';
+    eleEmailContent.style.width = '65%';
+    eleEmailContent.style.display = 'flex';
+
+    markFlag(id, 'read');
   }
 
-  // for (let i = 0; i < eleMarkFavorite.length; i++) {
-  //   eleMarkFavorite[i].addEventListener("click", markAsFavorite);
-  // }
-  // for (let i = 0; i < eleCards.length; i++) {
-  //   eleCards[i].addEventListener("click", openEmail);
-  // }
-
-  document.getElementById("email-list").addEventListener("click", openEmail);
+  document.getElementById('email-list').addEventListener('click', openEmail);
+  document.getElementById('filters').addEventListener('click', filterList);
+  document
+    .getElementById('email-content')
+    .addEventListener('click', markAsFavorite);
 
   init();
 })();
